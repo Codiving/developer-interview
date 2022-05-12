55254902fd047c0f1e8be32afce8a04064d69fde
Codiving <codivingmoon@gmail.com>
2022-05-12 23:10:56 +0900

    Quiz 컴포넌트 추가

---
+++ src/components/Quiz/Quiz.tsx
@@ -0,0 +1,121 @@
+import styled from "@emotion/styled";
+import { Typography } from "..";
+import { prefix } from "../../common";
+
+interface QuizProps {
+  type: string;
+  question: string[];
+  candidates: string[];
+  answer: string;
+  keywords: string[];
+  messages: string[];
+  index: number;
+  userAnswer: number;
+  onChangeAnswer: (index: number, answer: number) => void;
+  isSubmit: boolean;
+}
+
+const QuizContainer = styled("div", {
+  label: "QuizContainer"
+})(() => {
+  return {
+    display: "flex",
+    flexDirection: "column",
+    gap: "1em",
+    width: "100%",
+    padding: "1.2em",
+    borderRadius: 8,
+    backgroundColor: "#fafafa",
+    border: "1px solid #dad2d2"
+  };
+});
+
+const QuestionContainer = styled("div", {
+  label: "QuestionContainer"
+})(() => {
+  return {
+    display: "flex"
+  };
+});
+
+const Question = styled(Typography)(() => {
+  return {
+    marginRight: 20
+  };
+});
+
+const AnswerContainer = styled("div", {
+  label: "AnswerContainer"
+})(() => {
+  return {
+    display: "flex",
+    flexDirection: "column",
+    gap: "0.5em"
+  };
+});
+
+const AnswerList = styled("div", {
+  label: "AnswerList"
+})<{ selected: boolean }>(({ selected: _selected }) => {
+  const selected = _selected
+    ? {
+        outline: "2px solid",
+        backgroundColor: "#b0b0f0"
+      }
+    : {};
+  return {
+    padding: 5,
+    border: "1px solid",
+    borderRadius: 8,
+    backgroundColor: "white",
+    cursor: "pointer",
+    ...selected
+  };
+});
+
+const Answer = styled(Typography)(() => {
+  return {
+    fontSize: 20
+  };
+});
+
+const Quiz = (props: QuizProps) => {
+  const {
+    type,
+    question,
+    candidates,
+    answer,
+    keywords,
+    messages,
+    userAnswer,
+    index: upperIndex,
+    onChangeAnswer,
+    isSubmit
+  } = props;
+
+  return (
+    <QuizContainer>
+      <QuestionContainer>
+        <Question fontWeight={600} fontSize={24}>
+          Quiz:{" "}
+        </Question>
+        <Typography fontSize={20}>{question}</Typography>
+      </QuestionContainer>
+      <AnswerContainer>
+        {candidates.map((candidate, index) => (
+          <AnswerList
+            key={candidate}
+            onClick={() => {
+              onChangeAnswer(upperIndex, index);
+            }}
+            selected={index === userAnswer}
+          >
+            <Answer>{prefix[index]}.</Answer> <Answer>{candidate}</Answer>
+          </AnswerList>
+        ))}
+      </AnswerContainer>
+    </QuizContainer>
+  );
+};
+
+export default Quiz;
