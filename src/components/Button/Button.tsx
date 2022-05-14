896b787bf0bf274faf40add3e5ede25443067ce6
Codiving <codivingmoon@gmail.com>
2022-05-14 11:37:51 +0900

    Add Button Component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,64 @@
+import styled from "@emotion/styled";
+import { hexToRgbWithA } from "../../utils";
+import Typography from "../Typography";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  className?: string;
+  color?: string;
+  fontSize?: number;
+  selected?: boolean;
+}
+
+const ButtonContainer = styled("button", {
+  label: "ButtonContainer"
+})<{ color: string; selected: boolean }>(({ color, selected: _selected }) => {
+  const selected = _selected
+    ? { color: "white", backgroundColor: hexToRgbWithA(color, 0.5) }
+    : {};
+
+  return {
+    border: `1px solid ${color}`,
+    borderRadius: 8,
+    background: "none",
+    padding: 8,
+    cursor: "pointer",
+    color: color,
+    ...selected,
+    "&:hover": {
+      backgroundColor: hexToRgbWithA(color, 0.3),
+      color: "white"
+    }
+  };
+});
+
+const ButtonTypography = styled(Typography, {
+  label: "ButtonTypography"
+})<{ fontSize: number }>(({ fontSize }) => {
+  return {
+    fontSize: Boolean(fontSize) ? fontSize : 12
+  };
+});
+
+const Button = (props: ButtonProps) => {
+  const {
+    children,
+    className,
+    color = "#2196f3",
+    fontSize = 0,
+    selected = false
+  } = props;
+
+  return (
+    <ButtonContainer
+      className={className}
+      data-type="button"
+      color={color}
+      selected={selected}
+    >
+      <ButtonTypography fontSize={fontSize}>{children}</ButtonTypography>
+    </ButtonContainer>
+  );
+};
+
+export default Button;
