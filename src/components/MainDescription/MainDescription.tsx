8dc25260b260a26c7129053e651de4f464b10b06
Codiving <codivingmoon@gmail.com>
2022-05-15 18:54:37 +0900

    Update css

--- src/components/MainDescription/MainDescription.tsx
+++ src/components/MainDescription/MainDescription.tsx
@@ -7,7 +7,7 @@ const MainDescriptionContainer = styled("section", {
 })(() => {
   return {
     backgroundColor: "#e0e0ee",
-    padding: "3em"
+    padding: "5em"
   };
 });
 
@@ -21,27 +21,50 @@ const MainDescriptionContents = styled("div", {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    gap: "1.2em"
+    gap: "1.5em"
   };
+});
+
+const TypographyContainer = styled("div", {
+  label: "TypographyContainer"
+})(() => {
+  return {};
 });
 
 const MainDescription = () => {
   return (
     <MainDescriptionContainer>
       <MainDescriptionContents>
-        <div>
-          <Typography fontWeight={600} fontSize={24}>
+        <TypographyContainer>
+          <Typography fontWeight={600} fontSize={32}>
             프론트엔드 개발자 면접 준비
           </Typography>
           <Typography> 를 하고 계신가요?</Typography>
-        </div>
-        <div>
+        </TypographyContainer>
+        <TypographyContainer>
           <Typography>간단한 </Typography>
-          <Typography fontWeight={600} fontSize={24}>
+          <Typography fontWeight={600} fontSize={32}>
             객관식 면접
           </Typography>
           <Typography> 을 통해 나의 부족함을 확인해봅시다.</Typography>
-        </div>
+        </TypographyContainer>
+        {/* <TypographyContainer>
+          <Typography>문제를 풀고 </Typography>
+          <Typography fontWeight={600} fontSize={24}>
+            제출 즉시 오답 확인
+          </Typography>
+          <Typography>이 가능합니다.</Typography>
+        </TypographyContainer>
+        <TypographyContainer>
+          <Typography> 문제 </Typography>
+          <Typography fontWeight={600} fontSize={24}>
+            북마크 기능
+          </Typography>
+          <Typography> 을 통해 북마크한 문제만 풀 수 있습니다. </Typography>
+        </TypographyContainer>
+        <TypographyContainer>
+          <Typography>헷갈리는 문제들을 북마크하여 풀어보세요.</Typography>
+        </TypographyContainer> */}
       </MainDescriptionContents>
     </MainDescriptionContainer>
   );
