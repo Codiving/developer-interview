0491de42d7c315f96eb9fa32f2fd8a7e3a7ae921
Codiving <codivingmoon@gmail.com>
2022-05-15 19:58:12 +0900

    Add Header component

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,13 +1,60 @@
 import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+import { maxWidth } from "../../common";
+import { Typography } from "../../components";
 
 const HeaderContainer = styled("header", {
   label: "HeaderContainer"
 })(() => {
-  return {};
+  return {
+    width: "100%",
+    margin: "0 auto",
+    position: "sticky",
+    top: 0,
+    backgroundColor: "#fff",
+    borderBottom: "1px solid #efefef",
+    zIndex: 1
+  };
+});
+
+const HeaderContents = styled("div", {
+  label: "HeaderContents"
+})(() => {
+  return {
+    maxWidth: maxWidth,
+    width: "90%",
+    margin: "0 auto",
+    height: "100%",
+    display: "flex",
+    alignItems: "center"
+  };
+});
+
+const HeaderMainText = styled(Typography, {
+  label: "HeaderMainText"
+})(() => {
+  return {
+    fontSize: 32,
+    fontWeight: 600,
+    cursor: "pointer"
+  };
 });
 
 const Header = () => {
-  return <HeaderContainer>Header</HeaderContainer>;
+  const router = useRouter();
+
+  const onGoMainPage = useCallback(() => router.push("/"), [router]);
+
+  return (
+    <HeaderContainer>
+      <HeaderContents>
+        <HeaderMainText onClick={onGoMainPage} component="h1">
+          프론트엔드 Quiz
+        </HeaderMainText>
+      </HeaderContents>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
